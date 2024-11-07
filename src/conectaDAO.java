994d3883e38f6bd87b906ed9347bd53e7d27a58e
Ivo Brieda <ivobrieda@terra.com.br>
2024-11-07 17:52:46 -0300

    configurado os dados de conexão com o banco de dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -22,7 +22,9 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root","123456"); 
+          //conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=123456");
+          //conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cenaflix","root", "123456");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
